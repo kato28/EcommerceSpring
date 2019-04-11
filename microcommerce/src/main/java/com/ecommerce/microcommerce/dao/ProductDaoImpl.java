27ef602ae4ca2f1023bba0df117aeaabcf50eaaa
DARDOURI <ha.dardouri@gmail.com>
2019-04-11 16:08:58 +0200

    initial h2 database

--- microcommerce/src/main/java/com/ecommerce/microcommerce/dao/ProductDaoImpl.java
+++ microcommerce/src/main/java/com/ecommerce/microcommerce/dao/ProductDaoImpl.java
@@ -10,11 +10,11 @@ import java.util.List;
 public class ProductDaoImpl implements ProductDao {
 
     public static List<Product>products=new ArrayList<>();
-    static {
+    /*static {
         products.add(new Product(1, new String("Ordinateur portable"), 350));
         products.add(new Product(2, new String("Aspirateur Robot"), 500));
         products.add(new Product(3, new String("Table de Ping Pong"), 750));
-    }
+    }*/
 
     @Override
     public List<Product> findAll() {
