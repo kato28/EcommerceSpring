0bc55ca00abaf909ff11523ca9e436fea2a526d9
DARDOURI <ha.dardouri@gmail.com>
2019-04-11 10:10:37 +0200

    second update product controller

--- microcommerce/src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ microcommerce/src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -1,8 +1,6 @@
 package com.ecommerce.microcommerce.web.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
@@ -10,5 +8,10 @@ public class ProductController {
     @RequestMapping(value="/Produits", method= RequestMethod.GET)
     public String listeProduits() {
         return "Un exemple de produit";
+    }
+
+    @GetMapping(value = "/Produits/{id}")
+    public String afficherUnProduit(@PathVariable int id) {
+        return "Vous avez demandé un produit avec l'id  " + id;
     }
 }
