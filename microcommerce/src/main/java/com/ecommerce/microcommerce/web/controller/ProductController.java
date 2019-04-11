9df6d39dce2d6389ad54d697fbd743af9c898f8d
DARDOURI <ha.dardouri@gmail.com>
2019-04-11 11:52:46 +0200

    add post method to product controller

--- microcommerce/src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ microcommerce/src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -3,8 +3,11 @@ package com.ecommerce.microcommerce.web.controller;
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,4 +28,23 @@ public class ProductController {
 
         return productDao.findById(id);
     }
+
+    //ajouter un produit
+    @PostMapping(value = "/Produits")
+    public ResponseEntity<Void> ajouterProduit(@RequestBody Product product) {
+
+        Product productAdded =  productDao.save(product);
+
+        if (productAdded == null)
+            return ResponseEntity.noContent().build();
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(productAdded.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
 }
